12c3cdeb8309d41a8328def70cc488944d9edca3
NGÔ VĂN TUYẾN <ngovantuyen13042003@gmail.com>
2024-05-30 14:03:49 +0700

    feat: view list product

---
+++ DAPM/DAPM/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DAPM.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
